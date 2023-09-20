2de8cd1715287eb258eb1a1a92d37c368aa6b57b
manb11 <tjdrhd126@naver.com>
2023-09-20 05:36:08 +0000

    mango

---
+++ chap2/p29.py
@@ -0,0 +1,17 @@
+name = input("Hi. What's your name? ")
+age = int(input("And how old are you? "))
+weight = int(input("Okay, last question. How many pounds do you weight? "))
+print("")
+print("did you know that you're just 4 in dog years? ")
+print("")
+
+sec = 365*24*60*60*age # year, day, minutes, seconds
+
+print("But you're also over", sec, "seconds old.")
+
+print("If a small child were trying to get your attention, your name would become:", name*5)
+
+print("did you know that on the moon you would weigh only ", weight/6, " pounds?")
+
+print("But on the sun, you'd weigh", weight*280, " <but, ah... not for long>")
+
