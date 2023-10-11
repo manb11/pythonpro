05cee21ebebead9fe537242f3d9e7b71b4563639
manb11 <107660221+manb11@users.noreply.github.com>
2023-10-11 13:46:44 +0900

    Update prob2.py

--- chap6/prob2.py
+++ chap6/prob2.py
@@ -1,10 +1,32 @@
-print("   High Scores Keeper")
+print("\tHigh Scores Keeper")
 
-print("\t0 - Quit")
-print("\t1 - List")
-print("\t2 - Add a Score")
+scores = []
+while True:
+    
+    print("")
+    print("\t0 - Quit")
+    print("\t1 - List")
+    print("\t2 - Add a Score")
+    print("")
+    
+    num_input = int(input("Choice: "))
 
-num_input = int(input())
+    if num_input == 0:
+        break
 
-if num_input == 0:
-    print("System Out.")
+    if num_input == 2:
+        name_input = input("What is the player's name?: ")
+        score_input = int(input("What score did the player get?: "))
+        scores.append((score_input, name_input))
+    
+    if num_input == 1:
+        print("High Scores")
+        print("")
+        print("NAME\tSCORE")
+        
+        scores.sort()
+        scores.reverse()
+        
+        for entry in scores:
+            score_input, name_input = entry
+            print(name_input, "\t", score_input)
