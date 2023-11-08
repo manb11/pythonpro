00b798aa5ece4ad6ff00a86fb4f81031bb66a751
manb11 <tjdrhd126@naver.com>
2023-11-08 06:43:43 +0000

    10/8

--- chap8/prob3/prob3.py
+++ chap8/prob3/prob3.py
@@ -7,13 +7,19 @@ while True:
     a = input(">> ")
 
     if a !="Q":
-       text_file.write(a+"\n")
+       text_file.writelines(a)
     
     else: 
-        text_file = open("string.txt", "r")
-        output = text_file.readline()
-        print(output,("\n"))
+        text_file.close()
 
+        text_file = open("string.txt", "r")
+        output = text_file.readlines()
+       
+        text_file.close()
+        
+        for line in output:
+            print(line)
+        
         break
 
         
