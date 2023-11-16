e41128d57c763ad8dcbf5d3ad272b288ffbf5220
manb11 <tjdrhd126@naver.com>
2023-11-16 03:53:51 +0000

    11/16

--- chap8/prob1/open.py
+++ chap8/prob1/open.py
@@ -29,42 +29,35 @@ text_file.close()
 print("Reading one line at a time.")
 text_file = open("read_it.txt", "r")
 print(text_file.readline())
-
 print(text_file.readline())
-
 print(text_file.readline())
 print("")
 
 text_file.close()
-
 
 print("Reading the entire file into a list.")
 text_file = open("read_it.txt", "r")
 lines = text_file.readlines()
 print(lines)
-print("")
-
 
 text_file.close()
 
 text_file = open("read_it.txt", "r")
 
-a = input()
+a = int(input())  # Convert input to integer
 
-if a == 1:
-    print("Line 1")
-elif a == 2:
-    print("This is line 2")
-elif a == 3:
-    print("This makes this line 3")
-   
+for _ in range(a):
+    line = text_file.readline()
+    if not line:
+        break
+    print(line.strip(),"\n")
+
 text_file.close()
 
-
-print("Looping through the file, line by line.")
+print("\nLooping through the file, line by line.")
 text_file = open("read_it.txt", "r")
 for line in text_file:
-    print(line)
-print("")
+   print(line.strip(),"\n")
 
 text_file.close()
+
