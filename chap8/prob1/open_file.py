ca2aa45bf5dec12706ecf6aba0bb784da33141c4
manb11 <tjdrhd126@naver.com>
2023-11-16 03:45:07 +0000

    11/16

--- chap8/prob1/open_file.py
+++ chap8/prob1/open_file.py
@@ -7,11 +7,16 @@ print(text_file.read(1))
 print(text_file.read(5))
 print("")
 
+text_file.close()
+
+text_file = open("read_it.txt", "r")
+
 print("Reading the entire file at once.")
 whole_thing = text_file.read()
 print(whole_thing)
 print("")
 
+text_file.close()
  
 print("Reading characters from a line.")
 text_file = open("read_it.txt", "r")
@@ -19,6 +24,7 @@ print(text_file.readline(1))
 print(text_file.readline(5))
 print("")
 
+text_file.close()
 
 print("Reading one line at a time.")
 text_file = open("read_it.txt", "r")
@@ -29,6 +35,7 @@ print(text_file.readline())
 print(text_file.readline())
 print("")
 
+text_file.close()
 
 
 print("Reading the entire file into a list.")
@@ -37,6 +44,21 @@ lines = text_file.readlines()
 print(lines)
 print("")
 
+
+text_file.close()
+
+text_file = open("read_it.txt", "r")
+
+a = input()
+
+if a == 1:
+    print("Line 1")
+elif a == 2:
+    print("This is line 2")
+elif a == 3:
+    print("This makes this line 3")
+   
+text_file.close()
 
 
 print("Looping through the file, line by line.")
